f97489e0c04c7b4a3475d83f4c09d26ea9569278
Joe Winter <joe@joeventures.com>
2016-02-01 14:01:33 -0500

    all_primes function

---
+++ addaprime.php
@@ -0,0 +1,16 @@
+<?php
+
+function secret($number) {
+	return $number;
+}
+
+function all_primes() {
+	global $argv;
+	$input = intval($argv[1]);
+	if( $input < 3 ) die("Please input an integer larger than 2.");
+	$primes = array();
+	for( $i = 2; $i <= $input; $i = gmp_nextprime($i)) {
+		array_push($primes, gmp_strval($i));
+	}
+	return $primes;
+}
